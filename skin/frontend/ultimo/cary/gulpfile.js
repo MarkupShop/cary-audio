a94d43763b80c19ab87fd916924cfdb1baf89d94
Nick Rigby <nick.rigby@idfive.com>
2015-03-02 21:09:31 -0500

    Add placeholder SASS and JS for theme, with gulp file

---
+++ skin/frontend/ultimo/cary/gulpfile.js
@@ -0,0 +1,64 @@
+//Require
+var gulp = require('gulp'),
+		plumber	= require('gulp-plumber'),
+		sass = require('gulp-ruby-sass'),
+		jshint = require('gulp-jshint'),
+		uglify = require('gulp-uglify');
+
+//Set paths
+var paths =
+{
+	css:
+	{
+		src:	'sass/*.scss',
+		dest:	'css'
+	},
+	js:
+	{
+		src:	'js/*.js',
+		dest:	'js/build'
+	}
+};
+
+//Compile SASS
+gulp.task('compile-sass', function()
+{
+	return gulp
+		.src(paths.css.src)
+		.pipe(plumber())
+		.pipe(sass({
+			style: 'compressed',
+			precision: 8
+		}))
+		.pipe(gulp.dest(paths.css.dest));
+});
+
+//Lint JS
+gulp.task('lint-js', function()
+{
+    return gulp
+    	.src(paths.js.src)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+//Compile JS
+gulp.task('compile-js', function()
+{
+	return gulp
+		.src(paths.js.src)
+		.pipe(plumber())
+		.pipe(gulp.dest(paths.js.dest))
+		.pipe(uglify())
+		.pipe(gulp.dest(paths.js.dest));
+});
+
+//Create watch tasks
+gulp.task('watch', function()
+{
+    gulp.watch(paths.js.src, ['lint-js', 'compile-js']);
+    gulp.watch(paths.css.src, ['compile-sass']);
+});
+
+// Default Task
+gulp.task('default', ['compile-sass', 'lint-js', 'compile-js', 'watch']);
