a07db13cb4fe2ebd5ff3cc56bda4703b5aad788b
Nick Rigby <nick.rigby@gmail.com>
2015-11-16 22:45:10 -0500

    Oops: Fix spelling mistake

--- skin/frontend/markupshop/cary/js/templates/helpers.js
+++ skin/frontend/markupshop/cary/js/templates/helpers.js
@@ -5,7 +5,7 @@ Handlebars.registerHelper('date', function(options) {
   
   //Day
   var day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-      month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'Novemeber', 'December'];
+      month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   
   //Return formatted date
   return new Handlebars.SafeString(month[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear());
